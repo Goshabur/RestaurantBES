854cea0bf6afc6d26a49fd87d5cf5a9a2181f6d2
ereminaeliz <eremina.eliz2014@gmail.com>
2022-05-06 13:21:48 +0300

    fix logic and style, use dynamic

--- Liza/cart.h
+++ Liza/cart.h
@@ -1,156 +1,114 @@
 #pragma once
 
-#include <iostream>
-#include <map>
+#include "folly/dynamic.h"
+#include "folly/json.h"
 #include "fwd.h"
 #include "nlohmann/json.hpp"
+
+using folly::dynamic;
+using folly::parseJson;
+using nlohmann::json;
+using restbes::connectExec;
+using restbes::connectGet;
 
 namespace restbes {
 
 class Cart {
-private:
-    std::map<std::string, int> cart = {};
-
 public:
     id_t client_id;
     Cart() : client_id(0){};
 
-    [[nodiscard]] std::map<std::string, int> get_cart() const {
-        std::string get_cart = R"(SELECT "CART" FROM "CART" WHERE "CLIENT_ID" = )" +
-                               std::to_string(client_id);
-        try {
-            std::string j = restbes::connect_to_db_get(get_cart);
+    [[nodiscard]] json get_cart() const {
+        std::string cart = R"(SELECT "CART" FROM "CART" WHERE "CLIENT_ID" = )" +
+                           std::to_string(client_id);
 
-            auto json_parsed = nlohmann::json::parse(j);
-            for (auto &el : json_parsed.items()) {
-                std::cout << el.key() << ", quantity: " << el.value()
-                          << " pcs\n";
-            }
-        } catch (std::exception const &e) {
-            restbes::server_request_log << request << get_cart;
-            restbes::server_error_log << error << e.what();
-        }
+        dynamic responseJson =
+            dynamic::object("cart", cart)("total_price", total_price(cart));
 
-        std::cout << "Total sum: " << total_price() << '\n';
+        auto responseStr = folly::toJson(responseJson);
 
-        return cart;  // TODO: not needed if the client is logged in (?)
+        return responseStr;
     }
 
     void add(id_t dish_id) const noexcept {
-        //        ++cart[dish_id];
+        int quantity = 0;
+        nlohmann::json addToCart;
 
         try {
-            int quantity = std::stoi(restbes::connect_to_db_get(
-                R"(SELECT "CART" -> ')" + std::to_string(dish_id) +
-                R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
-                std::to_string(client_id)));
-
-            nlohmann::json j;
-            j[dish_id] = ++quantity;
-            std::string final = restbes::connect_to_db_get(
-                R"(SELECT "CART" || ')" + j.dump() +
-                R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
-                std::to_string(client_id));
-
-            restbes::connect_to_db_exec(R"(UPDATE "CART" SET "CART" = ')" +
-                                        final + R"(' WHERE "CLIENT_ID" = )" +
-                                        std::to_string(client_id));
+            quantity = std::stoi(
+                connectGet(R"(SELECT "CART" -> ')" + std::to_string(dish_id) +
+                           R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
+                           std::to_string(client_id)));
 
         } catch (...) {
-            nlohmann::json j;
-            j[dish_id] = 1;
-            std::string final = restbes::connect_to_db_get(
-                R"(SELECT "CART" || ')" + j.dump() +
-                R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
-                std::to_string(client_id));
-
-            restbes::connect_to_db_exec(R"(UPDATE "CART" SET "CART" = ')" +
-                                        final + R"(' WHERE "CLIENT_ID" = )" +
-                                        std::to_string(client_id));
+            // no such product in a cart
         }
+
+        addToCart[dish_id] = ++quantity;
+        std::string resultCart =
+            connectGet(R"(SELECT "CART" || ')" + addToCart.dump() +
+                       R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
+                       std::to_string(client_id));
+
+        connectExec(R"(UPDATE "CART" SET "CART" = ')" + resultCart +
+                    R"(' WHERE "CLIENT_ID" = )" + std::to_string(client_id));
     }
 
     void del(id_t dish_id) const noexcept {
-        std::string delete_from_cart = "delete_from_cart";
-        //        --cart[dish_id];
-
-        //        if (cart[dish_id] <= 0) {
-        //            cart.erase(dish_id);
-        //        }
-
         try {
-            int quantity = std::stoi(restbes::connect_to_db_get(
-                R"(SELECT "CART" -> ')" + std::to_string(dish_id) +
-                R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
-                std::to_string(client_id)));
+            int quantity = std::stoi(
+                connectGet(R"(SELECT "CART" -> ')" + std::to_string(dish_id) +
+                           R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
+                           std::to_string(client_id)));
 
-            nlohmann::json j;
-            j[dish_id] = --quantity;
-            std::string final;
+            nlohmann::json addToCart;
+            addToCart[dish_id] = --quantity;
+            std::string resultCart;
 
             if (quantity <= 0) {
-                final = restbes::connect_to_db_get(
+                resultCart = connectGet(
                     R"(SELECT "CART" - ')" + std::to_string(dish_id) +
                     R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
                     std::to_string(client_id));
-
             } else {
-                final = restbes::connect_to_db_get(
-                    R"(SELECT "CART" || ')" + j.dump() +
-                    R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
-                    std::to_string(client_id));
+                resultCart =
+                    connectGet(R"(SELECT "CART" || ')" + addToCart.dump() +
+                               R"(' FROM "CART" WHERE "CLIENT_ID" = )" +
+                               std::to_string(client_id));
             }
 
-            restbes::connect_to_db_exec(R"(UPDATE "CART" SET "CART" = ')" +
-                                        final + R"(' WHERE "CLIENT_ID" = )" +
-                                        std::to_string(client_id));
+            connectExec(R"(UPDATE "CART" SET "CART" = ')" + resultCart +
+                        R"(' WHERE "CLIENT_ID" = )" +
+                        std::to_string(client_id));
 
-            int cost = std::stoi(restbes::connect_to_db_get(
-                R"(SELECT "COST" FROM "CART" WHERE "CLIENT_ID" = )" +
-                std::to_string(client_id)));
+            connectExec(R"(UPDATE "CART" SET "COST" = )" +
+                        total_price(resultCart) + R"( WHERE "CLIENT_ID" = )" +
+                        std::to_string(client_id));
 
-            cost -= std::stoi(restbes::connect_to_db_get(
-                R"(SELECT "PRICE" FROM "DISH" WHERE "DISH_NAME" = ')" +
-                std::to_string(dish_id) + "'"));
-
-            restbes::connect_to_db_exec(
-                R"(UPDATE "CART" SET "COST" = )" + std::to_string(cost) +
-                R"( WHERE "CLIENT_ID" = )" + std::to_string(client_id));
-
-        } catch (std::exception const &e) {
-            restbes::server_request_log << request << delete_from_cart;
-            restbes::server_error_log << error << e.what();
+        } catch (...) {
+            // no such product in a cart
         }
     }
 
     void clear() const noexcept {
-        //        cart.clear();
+        connectExec(R"(UPDATE "CART" SET "CART" = '{}' WHERE "CLIENT_ID" = )" +
+                    std::to_string(client_id));
 
-        restbes::connect_to_db_exec(
-            R"(UPDATE "CART" SET "CART" = '{}' WHERE "CLIENT_ID" = )" +
-            std::to_string(client_id));
-
-        restbes::connect_to_db_exec(
-            R"(UPDATE "CART" SET "COST" = 0 WHERE "CLIENT_ID" = )" +
-            std::to_string(client_id));
+        connectExec(R"(UPDATE "CART" SET "COST" = 0 WHERE "CLIENT_ID" = )" +
+                    std::to_string(client_id));
     }
 
-    [[nodiscard]] int total_price() const {
+    [[nodiscard]] static std::string total_price(const json &cart) {
         int cost = 0;
 
-        std::string j = restbes::connect_to_db_get(
-            R"(SELECT "CART" FROM "CART" WHERE "CLIENT_ID" = )" +
-            std::to_string(client_id));
-
-        auto json_parsed = nlohmann::json::parse(j);
-        for (auto &el : json_parsed.items()) {
-            cost += std::stoi(restbes::connect_to_db_get(
+        for (auto &el : cart.items()) {
+            cost += std::stoi(connectGet(
                         R"(SELECT "PRICE" FROM "DISH" WHERE "DISH_NAME" = ')" +
                         el.key() + "'")) *
                     std::stoi(el.value().dump());
         }
 
-        return cost;
+        return std::to_string(cost);
     }
 };
 
